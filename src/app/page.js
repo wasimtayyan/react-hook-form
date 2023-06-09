8a0fd4871bebb1e290367eb1d2e94694b9ab8c87
wasim altayyan <wasimtayyan@gmail.com>
2023-06-09 22:19:15 +0300

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -57,7 +57,7 @@ export default function Home() {
           </p>
         </a>
 
-        <a
+        {/* <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
           target="_blank"
@@ -72,7 +72,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
-        </a>
+        </a> */}
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
